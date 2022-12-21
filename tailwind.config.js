c6f9dec92f36793976dbdb3e8b428d8213564b9f
JakubCukrowski <j.d.cukrowski@gmail.com>
2022-12-21 11:48:29 +0100

    Responsive design

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize: {
+      xs: "0.8rem"
+    },
+    extend: {
+      fontFamily: {
+        "manrope": ['"Manrope"', "cursive"]
+      },
+      colors: {
+        "very-dark-grayish-blue": "#48556A",
+        "desaturated-dark-blue": "#6D7F97",
+        "grayish-blue": "#9EAFC2",
+        "light-grayish-blue": "#ECF2F8"
+      }
+    },
+  },
+  plugins: [],
+}
